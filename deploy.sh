cdbc1e09d4e0d1f37eaaf25e97fb4f62c2785788
gxkai <30716965+gxkai@users.noreply.github.com>
2021-07-12 17:32:25 +0800

    go

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,7 @@
-#!/usr/bin/env sh
-
-# 当发生错误时中止脚本
-set -e
-
-# 构建
-yarn build
-# cd 到构建输出的目录下
-cd dist/spa
-
-# 部署到自定义域域名
-echo 'quasar.ksgxkai.top' > CNAME
-
-git init
-git add .
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-#git push -f git@github.com:TurboQuasar/vue3-quasar-template.github.io.git main
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:TurboQuasar/vue3-quasar-template.git main:gh-pages
-
-cd -
+rm -rf front
+mv dist front
+tar -czf front.tar front
+scp front.tar root@120.26.174.69:/usr/share/nginx/html/contracttest/front.tar
+rm -rf front.tar
+ssh root@120.26.174.69 'cd /usr/share/nginx/html/contracttest && rm -rf index.html && rm -rf static && tar -xzf front.tar && rm -rf front.tar'
+echo 'done. 👉 http://120.26.174.69/contracttest/front/'
